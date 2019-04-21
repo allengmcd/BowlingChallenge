10af6de583f3fba93bf59745842ee10093454620
Allen McDermott <alleng.mcd@gmail.com>
2019-04-21 08:31:12 -0500

    Initial commit

---
+++ BowlingChallenge/BowlingChallenge/ISimpleBowlingGame.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+
+namespace BowlingChallenge
+{
+    public interface ISimpleBowlingGame
+    {
+        // Called when a player completes a frame.
+        // This method will be called 10 times for a bowling game.
+        // The throws parameter provides the number of pins knocked down on each throw in the frame being recorded.
+        // The 10th frame may have 3 values.
+        void RecordFrame(params int[] throws);
+
+        // Called at the end of the game to get the final score.
+        int Score { get; }
+    }
+
+
+    public class BowlingGame : ISimpleBowlingGame
+    {
+        public int Score { get; internal set; }
+        private List<int> strikeSpareCounter; 
+        private int round;
+
+
+        /// <summary>
+        /// Constructor for the BowlingGame. Sets the Score to 0, round to 1, and clears any instances of strikes or spares
+        /// </summary>
+        public BowlingGame()
+        {
+            Score = 0;
+            round = 1;
+            strikeSpareCounter = new List<int>();
+        }
+
+
+        /// <summary>
+        /// Resets the Score, round, and any instances of strikes or spares
+        /// </summary>
+        public void NewGame()
+        {
+            Score = 0;
+            round = 1;
+            strikeSpareCounter = new List<int>();
+        }
+
+
+        /// <summary>
+        /// Called when a player completes a frame.
+        /// This method will be called 10 times for a bowling game.
+        /// The 10th frame may have 3 values.
+        /// </summary>
+        /// <param name="throws">The throws parameter provides the number of pins knocked down on each throw in the frame being recorded.</param>
+        public void RecordFrame(params int[] throws)
+        {
+            int roundScore = 0; // Initialize round score to 0
+            if (throws[0] == 10) // STRIKE!!!!
+            {
+                roundScore += 10; // Add ten to the score
+                AddStrikeSpareScores(throws[0]); // Add in strike or spare scores
+                strike(); // Call strike sequence
+
+                if(round == 10)
+                {
+                    AddStrikeSpareScores(throws[1]); // Add in strike or spare scores
+                }
+            }
+            else // No strike occurrd
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    roundScore += throws[i]; // Add some amount to the score
+                    AddStrikeSpareScores(throws[i]); // Add in strike or spare scores
+                }
+
+                if (roundScore == 10) // Spare!!
+                {
+                    spare(); // Call spare sequence
+                }
+            }
+            
+            if (round == 10 && roundScore >= 10) // Check if its the final round and if they can roll again
+            {
+                AddStrikeSpareScores(throws[2]); // Add in strike or spare scores
+            }
+
+            Score += roundScore; //Set score to total score
+            round++; // Implement the round counter by 1
+        }
+
+
+        /// <summary>
+        /// Initialize so that the next score will be counted
+        /// </summary>
+        private void spare()
+        {
+            int newSS = 1; // Set counter to 1 to count the next frame
+            strikeSpareCounter.Add(newSS);
+        }
+
+
+        /// <summary>
+        /// Initialize so that the next 2 scores will be counted
+        /// </summary>
+        private void strike()
+        {
+            int newSS = 2; // Set counter to 2 to count the next frame
+            strikeSpareCounter.Add(newSS);
+        }
+
+
+        /// <summary>
+        /// Adds in additional scores for any strikes or spares that have occurred
+        /// </summary>
+        /// <param name="frameScore"></param>
+        private void AddStrikeSpareScores(int frameScore)
+        {
+            for(int i = 0; i < strikeSpareCounter.Count; i++)
+            {
+                if(strikeSpareCounter[i] != 0)
+                {
+                    Score += frameScore;
+                    strikeSpareCounter[i]--;
+                }
+            }
+        }
+    }
+}
---
+++ BowlingChallenge/BowlingChallenge/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] rounds = new int[10][]
+            {
+                new int[] { 9, 0}, // Round #1
+                new int[] { 3, 5}, // Round #2
+                new int[] { 6, 1}, // Round #3
+                new int[] { 3, 6}, // Round #4
+                new int[] { 8, 1}, // Round #5
+                new int[] { 5, 3}, // Round #6
+                new int[] { 2, 5}, // Round #7
+                new int[] { 8, 0}, // Round #8
+                new int[] { 7, 1}, // Round #9
+                new int[] { 8, 1}, // Round #10
+            };
+            BowlingGame game = new BowlingGame();
+            //ISimpleBowlingGame game = new BowlingGame();
+
+            foreach (var round in rounds)
+            {
+                game.RecordFrame(round);
+            }
+
+            Console.WriteLine($"Your final score is: {game.Score}");
+            game.NewGame();
+
+            rounds = new int[10][]
+            {
+                    new int[] { 9, 0}, // Round #1
+                    new int[] { 3, 7}, // Round #2
+                    new int[] { 6, 1}, // Round #3
+                    new int[] { 3, 7}, // Round #4
+                    new int[] { 8, 1}, // Round #5
+                    new int[] { 5, 5}, // Round #6
+                    new int[] { 0, 10}, // Round #7
+                    new int[] { 8, 0}, // Round #8
+                    new int[] { 7, 3}, // Round #9
+                    new int[] { 8, 2, 8}, // Round #10
+            };
+
+            foreach (var round in rounds)
+            {
+                game.RecordFrame(round);
+            }
+
+            Console.WriteLine($"Your final score is: {game.Score}");
+            game.NewGame();
+
+            rounds = new int[10][]
+            {
+                    new int[] { 10, 0}, // Round #1
+                    new int[] { 3, 7}, // Round #2
+                    new int[] { 6, 1}, // Round #3
+                    new int[] { 10 }, // Round #4
+                    new int[] { 10 }, // Round #5
+                    new int[] { 10 }, // Round #6
+                    new int[] { 2, 8 }, // Round #7
+                    new int[] { 9, 0}, // Round #8
+                    new int[] { 7, 3}, // Round #9
+                    new int[] { 10, 10, 10}, // Round #10
+            };
+
+            foreach (var round in rounds)
+            {
+                game.RecordFrame(round);
+            }
+
+            Console.WriteLine($"Your final score is: {game.Score}");
+            Console.ReadKey();
+        }
+    }
+}
